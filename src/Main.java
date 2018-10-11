1269b7fa01da8e81064e840fc8ea3e2f8b4e4ade
Joachimstaller Alexander Lothar <1810653261@stud.fh-kufstein.ac.at>
2018-10-11 15:51:35 +0200

    herkunft hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
         int alter = 18;
         String nachname = "Joachimstaller";
         int jahrgang = 2000;
-        System.out.println("Hello " + vorname + " " + nachname + " du bist " + alter + " Jahre alt" + " und jahrgang geboren!");
+        String herkunft = "Rosenheim";
+        System.out.println("Hello " + vorname + " " + nachname + " du bist " + alter + " Jahre alt" + " und " + jahrgang + " geboren" + " und kommst aus " + herkunft + "!");
     }
 
 }
