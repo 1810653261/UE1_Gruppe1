3517a2bc187a52e05cf6723b9900e7d497d5430a
Joachimstaller Alexander Lothar <1810653261@stud.fh-kufstein.ac.at>
2018-10-11 15:17:54 +0200

    alter hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        String vorname = "alex"
-        System.out.println("Hello" + vorname +"!");
+        String vorname = "Alex";
+        int alter = 18;
+        System.out.println("Hello " + vorname + " du bist " + alter + " Jahre alt!");
     }
 
 }
